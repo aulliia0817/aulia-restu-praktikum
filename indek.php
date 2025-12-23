1612c35c4e78e5eea9834c21bce8cdb4dfacdbf7
aulliia0817 <auliarestumahardika@gmail.com>
2025-12-24 02:31:27 +0700

    Update index.php
    menambahkan code pada program untuk menyesuaikan modul12

--- indek.php
+++ indek.php
@@ -11,14 +11,28 @@
             <h3>www.unipma.ac.id</h3>
         </div>
         <br/>
+        <?php 
+        if(isset($GET['pesan'])){
+            $pesan = $_GET['pesan'];
+            if($pesan == "input"){
+                echo "Data berhasil di input.";
+            } else if($pesan == "update"){
+                echo "Data berhasil di update.";
+            } else if($pesan == "hapus"){
+                echo "Data berhasil di hapus.";
+            }
+        }
+        ?>
         <br/>
-        <h3>Data Mahasiswa</h3>
+        <a class="tombol" href="input.php">+ Tambah Data</a>
+        <h3>Data User</h3>
         <table border="1" class="table">
             <tr>
                 <th>NPM</th>
                 <th>Nama</th>
                 <th>Alamat</th>
                 <th>Kelas</th>
+                <th>Opsi</th>
             </tr>
             <?php 
             include "koneksi.php";
@@ -31,8 +45,13 @@
                 <td><?php echo $data['nama']; ?></td>
                 <td><?php echo $data['alamat']; ?></td>
                 <td><?php echo $data['kelas']; ?></td>
+                <td>
+                    <a class="edit" href="edit.php?npm=<?php echo $data['npm']; ?>">Edit</a>
+                    <a class="hapus" href="hapus.php?npm=<?php echo $data['npm']; ?>">Hapus</a>
+                </td>
             </tr>
             <?php } ?>
         </table>
     </body>
+
 </html>
